67aad5e9d922aa02ba5b8c04638e34d8f5fa6a06
Henrique Leite <henriqueleite616@gmail.com>
2019-06-07 11:45:22 -0300

    Organizando o Projeto

---
+++ ADD/heater.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '
+<section id="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="index.php"><img src="ADD/book.png" alt="Logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">HOME</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</section>
+';
+
+?>
